26b136d9a22d79a85fe20af2b9585a17b13e2e4e
Roderic Page <rdmpage@gmail.com>
2022-07-11 12:09:35 +0100

    Add BHL harvest calls

---
+++ itemarticles.php
@@ -0,0 +1,169 @@
+<?php
+
+error_reporting(E_ALL);
+
+require_once (dirname(__FILE__) . '/elastic.php');
+
+
+$ItemID = -1;
+
+if (isset($_GET['item']))
+{
+	$ItemID = $_GET['item'];
+}
+
+// handle possible synonym
+if (isset($_GET['ItemID']))
+{
+	$ItemID = $_GET['ItemID'];
+}
+
+
+$callback = '';
+
+if (isset($_GET['callback']))
+{
+	$callback = $_GET['callback'];
+}
+
+$obj = new stdclass;
+$obj->ItemID = $ItemID;
+$obj->articles = array();
+
+// get records since timestamp
+$query_json = 		
+	'{
+	"size": 1000,
+	"query": {
+		"term": {
+			"search_data.item": {
+				"value": <ITEM>
+			}
+		}
+	}
+}';
+
+$query_json = str_replace('<ITEM>', (Integer)$ItemID, $query_json);
+		
+$resp = $elastic->send('POST', '_search?pretty', $post_data = $query_json);
+
+$response_obj = json_decode($resp);
+
+// print_r($response_obj);
+
+foreach ($response_obj->hits->hits as $hit)
+{
+	// build article that BHL will import
+	
+	$article = new stdclass;
+	$article->reference_id = str_replace('biostor-', '', $hit->_source->id);
+		
+	foreach ($hit->_source->search_result_data->csl as $k => $v)
+	{
+		switch ($k)		
+		{
+			case 'type':
+				switch ($v)
+				{
+					case 'article-journal':
+						$article->genre = 'article';
+						break;
+				
+					default:
+						$article->genre = $v;
+						break;
+				}
+				break;
+		
+			case 'title':
+			case 'volume':
+			case 'issue':
+				$article->{$k} = $v;
+				break;
+				
+			case 'author':
+				$article->authors = array();
+				foreach ($v as $a)
+				{
+					$author = new stdclass;
+					$author->lastname = $a->family;
+					$author->forename = $a->given;
+					
+					$article->authors[] = $author;
+				}
+				break;
+				
+			case 'container-title':
+				$article->{'secondary-title'} = $v;
+				break;
+				
+			case 'page':
+				if (preg_match('/(.*)-(.*)/', $v, $m))
+				{
+					$article->spage = $m[1];
+					$article->epage = $m[2];						
+				}
+				else
+				{
+					$article->spage = $v;
+				}
+				break;
+				
+			case 'issued':
+				$article->year = $v->{'date-parts'}[0][0];
+				
+				// ISO date
+				$article->date = $v->{'date-parts'}[0][0];
+				if (isset($v->{'date-parts'}[0][1]))
+				{
+					$article->date .= '-' . str_pad($v->{'date-parts'}[0][1], 2, '0', STR_PAD_LEFT);
+					
+					if (isset($v->{'date-parts'}[0][2]))
+					{
+						$article->date .= '-' . str_pad($v->{'date-parts'}[0][2], 2, '0', STR_PAD_LEFT);
+					}					
+				}				
+				break;
+							
+			case 'ISSN':
+				$article->issn = $v[0];
+				break;
+			
+			case 'DOI':
+				$article->doi = $v;
+				break;
+				
+	
+			default:
+				break;
+		}
+	}
+	
+	// Dates created and modified
+	$article->created = date(DATE_ISO8601, $hit->_source->search_data->created);
+	$article->modified = date(DATE_ISO8601, $hit->_source->search_data->modified);
+	
+	// BHL pages	
+	$article->bhl_pages = $hit->_source->search_result_data->bhl_pages;
+	$article->PageID = $article->bhl_pages[0];
+	
+	$obj->articles[] = $article;
+}
+
+
+
+header('Content-type: text/plain');
+if ($callback != '')
+{
+	echo $callback .'(';
+}
+
+echo json_encode($obj, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+
+if ($callback != '')
+{
+	echo ')';
+}
+
+?>
+
---
+++ itemsince.php
@@ -0,0 +1,83 @@
+<?php
+
+error_reporting(E_ALL);
+
+require_once (dirname(__FILE__) . '/elastic.php');
+
+$since = date('Y-m-d', time());
+
+if (isset($_GET['since']))
+{
+	$since = $_GET['since'];
+	
+	// Check format
+	if (preg_match('/^[0-9]{4}\-[0-9]{2}\-[0-9]{2}$/', $since))
+	{
+	}
+	else
+	{
+		$since = date('Y-m-d', time());
+	}
+}
+
+$callback = '';
+
+if (isset($_GET['callback']))
+{
+	$callback = $_GET['callback'];
+}
+
+$obj = new stdclass;
+$obj->since = $since;
+$obj->count = 0;
+$obj->limit = 1000;
+$obj->items = array();
+
+$timestamp = strtotime($since);
+
+// get records since timestamp
+$query_json = 		
+	'{
+	"size": 1000,
+	"_source" :[ "search_data.item"],
+	"query": {
+		"range": {
+			"search_data.created": { 
+				"gte": <TIMESTAMP> 
+            }		
+        }
+	}
+}';
+
+$query_json = str_replace('<TIMESTAMP>', $timestamp, $query_json);
+		
+$resp = $elastic->send('POST', '_search?pretty', $post_data = $query_json);
+	
+
+$response_obj = json_decode($resp);
+
+//print_r($response_obj);
+
+foreach ($response_obj->hits->hits as $hit)
+{
+	$obj->items[] = $hit->_source->search_data->item;
+}
+
+$obj->count = count($obj->items);
+
+
+header('Content-type: text/plain');
+if ($callback != '')
+{
+	echo $callback .'(';
+}
+
+echo json_encode($obj, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+
+if ($callback != '')
+{
+	echo ')';
+}
+
+?>
+
