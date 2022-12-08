bdf2cc4cd2aa7e59bc3ed34908193afb8510a9a5
Roderic Page <rdmpage@gmail.com>
2022-12-08 14:11:22 +0000

    Tiles no longer clip overlapping markers from adjacent tiles

--- tile.php
+++ tile.php
@@ -6,14 +6,13 @@ require_once (dirname(__FILE__) . '/elastic.php');
 
 // tile request will supply x,y and z (zoom level)
 
-// Need to figure out how to include points from outside a tile that are
-// clipped, this is tricky
 
-$x = 0;
-$y = 0;
-$zoom = 0;
+$x 		= 0;
+$y 		= 0;
+$zoom 	= 0;
 
 define ('TILE_SIZE', 256);
+define ('MARKER_SIZE', 10);
 
 $debug = false;
 //$debug = true;
@@ -51,7 +50,9 @@ function xyz_to_lat_long($x, $y, $zoom)
 }
 
 //----------------------------------------------------------------------------------------
-// Convert tile to corresponding bounding box in lat,lon coordinates
+// Convert tile to corresponding bounding box in lat,lon coordinates. We inflate 
+// this by the size of the marker so that we can draw parts of markers whose centre
+// is outside the tile (otherwise markers near tile edges will be clipped)
 function xyz_to_bounding_box($x, $y, $zoom)
 {
 	$obj = new stdclass;
@@ -69,19 +70,13 @@ function xyz_to_bounding_box($x, $y, $zoom)
 	$obj->bottom_right->lon = $lon_lat[0];
 	
 	// inflate
-	if (0)
+	if (1)
 	{
-		//print_r($obj);
-		
 		$dx = (abs($obj->top_left->lon - $obj->bottom_right->lon))/ TILE_SIZE;
-		$dx *= 8;
+		$dx *= MARKER_SIZE;
 		
-		//echo $dx;
-	
 		$dy = (abs($obj->top_left->lat - $obj->bottom_right->lat))/ TILE_SIZE;
-		$dy *= 8;
-		
-		//echo $dy;
+		$dy *= MARKER_SIZE;
 	
 		$obj->top_left->lat = min(90, $obj->top_left->lat + $dy);
 		$obj->bottom_right->lat = max(-90, $obj->bottom_right->lat - $dy);
@@ -89,14 +84,13 @@ function xyz_to_bounding_box($x, $y, $zoom)
 		$obj->top_left->lon = max(-180, $obj->top_left->lon - $dy);
 		$obj->bottom_right->lon = min(180, $obj->bottom_right->lon + $dy);
 		
-		//print_r($obj);
 	}
 	return $obj;
 }
 
 //----------------------------------------------------------------------------------------
 // Convert (lat,lon) pair to x,y coordinates within a tile 
-function lat_lon_to_xy($lon_lat, $zoom)
+function lat_lon_to_xy($lon_lat, $tile_x, $tile_y, $zoom)
 {
 	$xy = array();
 	
@@ -105,31 +99,76 @@ function lat_lon_to_xy($lon_lat, $zoom)
 	$x_pos = ($lon_lat[0] + 180)/360 * $n;
 	$x = floor($x_pos);
 	
-	$relative_x = round(TILE_SIZE * ($x_pos - $x));
+	/*
+	if ($x < $tile_x)
+	{
+		$x_pos -= TILE_SIZE;
+	}
+	elseif ($x > $tile_x)
+	{
+		$x_pos += TILE_SIZE;
+	}
+	*/
+
+	$relative_x = round(TILE_SIZE * ($x_pos - $tile_x));
 	
 	$y_pos = (1 - log(tan($lon_lat[1] * M_PI / 180.0) + 1/cos($lon_lat[1] * M_PI / 180.0))/M_PI)/2 * $n;
+	
+	/*
+	$y_pos =
+	
+		(1 - 
+			log(
+				tan($lon_lat[1] * M_PI / 180.0)
+				 + 
+				 1/cos($lon_lat[1] * M_PI / 180.0)
+				)
+				/M_PI
+		)
+		/
+		2
+		 * $n;
+	
+	
+	
+	
+	
+	*/
+	
+	// (1 - LOG(TAN(RADIANS(x) + 1/COS(RADIANS(x)))/PI())/2
+	
 	$y = floor($y_pos);
 	
+	/*
+	if ($y < $tile_y)
+	{
+		$y_pos -= TILE_SIZE;
+	}
+	elseif ($y > $tile_y)
+	{
+		$y_pos += TILE_SIZE;
+	}
+	*/
 	
-	$relative_y = round(TILE_SIZE * ($y_pos - $y));
+	
+	$relative_y = round(TILE_SIZE * ($y_pos - $tile_y));
 
 	$xy = array($relative_x , $relative_y);
+	
+	//echo $lon_lat[0] . "|" . $lon_lat[1] . "|" . $x_pos . '|' . $y_pos . "|" . $x . "|" . $y . '|' . $relative_x . '|' . $relative_y . "\n" ;
 	
 	return $xy;
 }
 
 
 //----------------------------------------------------------------------------------------
-// Query Elastic to get dots on map 
 
 
 // Create SVG tile
 $xml = '<?xml version="1.0" encoding="UTF-8"?>
 <svg xmlns:xlink="http://www.w3.org/1999/xlink" 
 xmlns="http://www.w3.org/2000/svg" 
-width="' . TILE_SIZE . '" height="' . TILE_SIZE . '" overflow="visible"
-
- >
+width="' . TILE_SIZE . '" height="' . TILE_SIZE . '">
    <style type="text/css">
 	  <![CDATA[     
 	  ]]>
@@ -143,7 +182,6 @@ if ($debug)
 	$xml .= '<text x="10" y="15">' . $x . ' ' . $y . ' ' . $zoom . '</text>';		
 }
 	 
-$marker_size = 8;
 $marker_shape = 'circle';
 //$marker_shape = 'square';
 
@@ -214,54 +252,61 @@ foreach ($response_obj->hits->hits as $hit)
 	foreach ($hit->_source->search_data->geometry->coordinates as $lon_lat)
 	{
 		// We only want to display points in coordinates array that are within the bounds 
-		// of this tile. Because a BioStor JSON document has a set of points, not all need
-		// be within this tile.
+		// of this tile. Because a BioStor JSON document has a set of all points in 
+		// that document, we need to filter out those points.
 		
-		$show = false;
-		
+		$show = false;		
 		if ($lon_lat[0] >= $min_lon_lat[0] && ($lon_lat[0] <= $max_lon_lat[0]))
 		{
 			if ($lon_lat[1] >= $min_lon_lat[1] && ($lon_lat[1]  <= $max_lon_lat[1]))
 			{
 				$show = true;
-				
-				//echo $lon_lat[0] . ' ' . $lon_lat[1] . "\n";
 			}	
 		}		
 		
 		if ($show)
 		{
-			$xy = lat_lon_to_xy	($lon_lat, $zoom);
+			$xy = lat_lon_to_xy	($lon_lat, $x, $y, $zoom);
 	
 			$x_pos = $xy[0];
 			$y_pos = $xy[1];
-			
-			//echo $lon_lat[0] . '|' . $lon_lat[1] . '|' . $x_pos . '|' . $y_pos . "\n" ;
-	
+
 			switch ($marker_shape)
 			{
 				case 'square':
-					$offset = $marker_size / 2;
-					$xml .= '<rect id="dot" x="' . ($x_pos - $offset) . '" y="' . ($y_pos - $offset) . '" width="' . $marker_size . '" height="' . $marker_size . '" style="stroke-width:1;"';			
+					$offset = MARKER_SIZE / 2;
+					$xml .= '<rect id="dot" x="' . ($x_pos - $offset) . '" y="' . ($y_pos - $offset) . '" width="' . MARKER_SIZE. '" height="' . MARKER_SIZE . '" style="stroke-width:1;"';			
 					break;
 	
 				case 'circle':
 				default:
-					$radius = $marker_size / 2;
+					$radius = MARKER_SIZE / 2;
 					$offset = 0;
-					$xml .= '<circle id="dot" cx="' . ($x_pos - $offset) . '" cy="' . ($y_pos - $offset) . '" r="' . $radius . '" style="stroke-width:1;"';
+					$xml .= '<circle id="dot" cx="' . ($x_pos - $offset) . '" cy="' . ($y_pos - $offset) . '" r="' . $radius . '" style="stroke-width:0.5;"';
 					break;
 			}
 		
-			$fill = 'rgba(0,0,0,0.5)';
-			$fill = 'rgb(208,104,85)'; // Canadensys
-	
+			// styles
+			
+			if (1)
+			{
+				// Canadensys
+				$fill 		= 'rgb(208,104,85)';
+				$stroke 	= 'rgb(38,38,38)';
+				$opacity	= '1.0';				
+			}
+
+			if (0)
+			{
+				$fill 		= 'rgb(255,0,0)';
+				$stroke 	= 'none';
+				$opacity	= '0.5';				
+			}
+			
 			$xml .= ' fill="'. $fill . '"';
-	
-			//$xml .= ' opacity="0.7"';
-		
-		
-			$xml .= ' stroke="rgb(38,38,38)"'; // Canadensys
+			$xml .= ' stroke="'. $stroke . '"';
+			$xml .= ' opacity="'. $opacity . '"';
+			
 			$xml .= '/>';	
 		}
 	}
